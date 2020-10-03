b9a4673719e5baba91d4c271da4e61373573d994
k1te <kmiller343@gmail.com>
2020-10-03 18:46:42 -0500

    formatted docs

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 $(document).ready(function () {
-
   //capture time from momentjs
   const time = moment().format("MMMM do YYYY");
 
   //display time in current day HTML section
   let currentD = $("#currentDay");
   currentD.text(time);
-  
+
   let currentH24 = parseInt(moment().format("kk")); // gives hour in 24 hour format
   let currentH12 = parseInt(moment().format("H")); // gives hour in 12 hour format
-
-
-  
 
   //for loop to build daily calendar rows beginning at 9AM to 5PM, 12 + 5 = 17
   for (let hour = 9; hour <= 17; hour++) {
@@ -27,29 +23,21 @@ $(document).ready(function () {
     //hour span
     const hourDaySpan = $("<span>");
     hourDaySpan.addClass("hour");
-    
 
     //hour-index/hour display conversion takes the hour-index attr from rowDiv, converts to actual hour and dipslays PM for anything after 12, sets equal to index for AM as the hours match
     let plannerHour = 0;
     let mornAft = "";
-    // if (hour > 12) {
-    //   plannerHour = hour - 12;
-    //   mornAft = "pm";
-    // } else {
-    //   plannerHour = hour;
-    //   mornAft = "am";
-    // }
+
     if (hour > 12) {
-        plannerHour = hour -12;
-        mornAft = "pm";
+      plannerHour = hour - 12;
+      mornAft = "pm";
     } else if (hour === 12) {
-        plannerHour = hour;
-        mornAft = "pm";
+      plannerHour = hour;
+      mornAft = "pm";
     } else if (hour < 12) {
-        plannerHour = hour;
-        mornAft = "am";
+      plannerHour = hour;
+      mornAft = "am";
     }
-    
 
     //add hour display to hourDaySpan
     hourDaySpan.text(plannerHour + mornAft);
@@ -61,17 +49,17 @@ $(document).ready(function () {
     userInput = $("<textarea>");
     userInput.attr("type", "text");
     if (hour < currentH24) {
-        userInput.addClass("past");
+      userInput.addClass("past");
     } else if (hour === currentH24) {
-        userInput.addClass("present");
+      userInput.addClass("present");
     } else {
-        userInput.addClass("future");
+      userInput.addClass("future");
     }
     userInput.addClass("description");
     userInput.attr("ppf-index", hour);
     userInputDiv = $("<div>");
     userInputDiv.addClass("col-md-9 pr-0 mr-0 pl-0 ml-0");
-    
+
     rowD.append(userInputDiv);
     userInputDiv.append(userInput);
 
@@ -86,26 +74,5 @@ $(document).ready(function () {
     //add save button to row
     rowD.append(saveBtnDiv);
     saveBtnDiv.append(saveBtn);
-
-    // updatePPF(currentH24, plannerHour);
-    // console.log(plannerHour);
-    // console.log(hour);
-    // console.log(currentH24);
-
   }
-// update past present or future based on time of day - need to utilize H24 and H12 based on plannerHour
-//   function updatePPF (currentH24, plannerHour) {
-//       if (plannerHour == currentH24) {
-//           userInput.addClass("present");
-//       } else if (plannerHour < currentH24) {
-//           userInput.addClass("past");
-//       } else if (plannerHour > currentH24) {
-//           userInput.addClass("future");
-//       }
-//   }
-
-
-
-
-
 });
