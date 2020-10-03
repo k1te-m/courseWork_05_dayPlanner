8eedcff0ffad9ea98f88e4c19f8dcc8106d7b778
k1te <kmiller343@gmail.com>
2020-10-03 13:48:49 -0500

    successfully added initial rows for planner via jquery

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ const time = moment().format('MMMM do YYYY');
 let currentD = $("#currentDay");
 currentD.text(time);
 
+//build daily calendar rows
+for (let hour = 9; hour <=17; hour++) {
 
+    //build rows
+    let rowD = $("<div>");
+    rowD.addClass("row calendar-row");
+    rowD.attr("hour-index", hour);
+    rowD.text("test");
+
+    $("#planner").append(rowD);
+
+}
 
 
 
